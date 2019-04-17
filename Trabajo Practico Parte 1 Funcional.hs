2a0f9ee186f7dd835eeb57c4e5b2bf0b7335d345
Marcos Donner <marcos.donner.97@gmail.com>
2019-04-17 00:25:15 -0300

    Test realizados, pero no compilan por un error

--- Trabajo Practico Parte 1 Funcional.hs
+++ Trabajo Practico Parte 1 Funcional.hs
@@ -3,13 +3,37 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
 import Data.List -- para los metodos coleccionables que no vienen en la guia de lenguaje
 import Data.Maybe -- por si llegan a usar un metodo de coleccion y devuelva Nothing or justElements
-import Text.Show.Functions --
--- import Test.Hspec -- para usar los test
+import Text.Show.Functions -- para que las funciones dentro del data se vean <function>
+import Test.Hspec -- para usar los test
 --Para que funcionen los test instalar lo siguiente mediante la terminal de windows
 --cabal update
 --cabal install hspec
---nueva prueba a ver si funca bien.
---Prueba a ver si reconoce mi nombre cuando hago el commit.
+
+--Test basados en los casos de prueba y en la programación TTD -> Test-Driven-Development (desarrollo dirigido por tests)
+
+ejecutarTest = hspec $ do
+	describe "Test del punto 3.1" $ do
+		it "Rocha despues de hacer su truco queda en 500 de nafta" $ (nivelNafta.deReversaRocha) rochaMcQueen `shouldBe` 500
+		it "Velocidad de biankerr despues de hacer su truco es 40" $ (velocidad.impresionar) biankerr `shouldBe` 40
+		it "Velocidad de gushtav despues de hacer su truco es 145" $ (velocidad.nitro) gushtav `shouldBe` 145
+		it "La enamorade de rodra cambia por Petra con su truco" $ (enamorade.(fingirAmor "petra")) rodra `shouldBe` "petra"
+	describe "Test del punto 3.2" $ do
+		it "Velocidad de rocha despues de incrementar velocidad es 15" $ (velocidad.incrementarVelocidad) rochaMcQueen `shouldBe` 15
+		it "Velocidad de biankerr despues de incrementar velocidad es 35" $ (velocidad.incrementarVelocidad) biankerr `shouldBe` 35
+		it "Velocidad de gushtav despues de incrementar velocidad es 35" $ (velocidad.incrementarVelocidad) gushtav `shouldBe` 160
+		it "Velocidad de rodra despues de incrementar velocidad es 35" $ (velocidad.incrementarVelocidad) rodra `shouldBe` 70
+	describe "Test del punto 3.3" $ do
+		it "Rocha puede realizar su truco" (puedeRealizarTruco rochaMcQueen)
+		it "Gushtav no puede usar su truco" (not(puedeRealizarTruco gushtav))
+		it "Rodra no puede usar su truco" (not(puedeRealizarTruco rodra))
+  describe "Test del punto 3.4" $ do
+		it "La nafta de rocha despues de comboLoco debe ser 500" $ (nivelNafta.comboLoco) rochaMcQueen `shouldBe` 500
+		it "La velocidad de rocha despues de comboLoco debe ser 15" $ (velocidad.comboLoco) rochaMcQueen `shouldBe` 15
+		it "La velocidad de Rodra despues de usar queTrucazo con murcielago debe ser 80" $ (velocidad.queTrucazo "murcielago") rodra `shouldBe` 80
+		it "La velocidad de gushtav despues de usar turbo es 2130" $ (velocidad.turbo) gushtav `shouldBe` 2130
+		it "La nafta de gushtav despues de usar turbo es 0" $ (nivelNafta.turbo) gushtav `shouldBe` 0
+		it "La velocidad de rodra despues de usar turbo es 50" $ (velocidad.turbo) rodra `shouldBe` 50
+		it "La nafta de rodra despues de usar turbo es de 0" $ (nivelNafta.turbo) rodra `shouldBe` 0
 
 -- PUNTO 1
 
