cb43cd19c38f8104f7a587bbb9c68cf3d6ad42ad
Alex <alexmclm@gmail.com>
2019-05-28 18:49:42 -0300

    agregue base del universo

---
+++ Logico/tpLogico.pl
@@ -0,0 +1,77 @@
+%% candidato (Nombre, Partido)
+candidato(frank,rojo).
+candidato(claire,rojo).
+candidato(garret, azul).
+candidato(jackie, amarillo).
+candidato(linda, azul).
+candidato(catherine,rojo).
+candidato(seth,amarillo).
+candidato(heather,amarillo).
+
+%% edad (Nombre, Edad).
+edad(frank,50).
+edad(claire,52).
+edad(garret,64).
+edad(peter,26).
+edad(jackie,38).
+edad(linda,30).
+edad(catherine,59).
+edad(heather,50). %% por que reste 2019-1969 , creo que prolog no podia hacer operaciones asi no?
+
+%% postulacion(Partido,Provincia)
+
+postulacion(azul,buenosAires).
+postulacion(azul,chaco).
+postulacion(azul,tierraDelFuego).
+postulacion(azul,sanLuis).
+postulacion(azul,neuquen).
+postulacion(rojo,buenosAires).
+postulacion(rojo,santaFe).
+postulacion(rojo,cordoba).
+postulacion(rojo,chubut).
+postulacion(rojo,tierraDelFuego).
+postulacion(rojo,sanLuis).
+postulacion(amarillo,chaco).
+postulacion(amarillo,formosa).
+postulacion(amarillo,tucuman).
+postulacion(amarillo,salta).
+postulacion(amarillo,santaCruz).
+postulacion(amarillo,laPampa).
+postulacion(amarillo,corrientes).
+postulacion(amarillo,misiones).
+postulacion(amarillo,buenosAires).
+
+%% habitantes(provincia, habitantes).
+
+habitantes(buenosAires,15355000).
+habitantes(chaco,1143201).
+habitantes(tierraDelFuego,160720).
+habitantes(sanLuis,489255).
+habitantes(neuquen,637913).
+habitantes(santaFe,3397532).
+habitantes(cordoba,3567654).
+habitantes(chubut,577466).
+habitantes(formosa,527895).
+habitantes(tucuman,1687305).
+habitantes(salta,1333365).
+habitantes(santaCruz,273964).
+habitantes(laPampa,349299).
+habitantes(corrientes,992595).
+habitantes(misiones,1189446).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
